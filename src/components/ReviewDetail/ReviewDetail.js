907d58fbd30fe9c9f2d59f31b04bc05541a9fb90
Jamshedjisan <jamshedjisan76@gmail.com>
2022-04-05 15:53:37 +0600

    radme file changed

--- src/components/ReviewDetail/ReviewDetail.js
+++ src/components/ReviewDetail/ReviewDetail.js
@@ -3,16 +3,14 @@ import React from "react";
 const ReviewDetail = (props) => {
   const { Name, Rating, image, Comment } = props.product;
   return (
-    <div>
-      <div className="border">
-        <div className="w-1/4 ml-32">
-          <img src={image} alt="" />
-        </div>
-        <div className="w-2/3">
-          <h3 className="text-2xl font-bold ">Name: {Name}</h3>
-          <h4 className="text-xl font-semibold">Ratings: {Rating}</h4>
-          <p className="text-xl">Comment: {Comment}</p>
-        </div>
+    <div className="">
+      <div className="w-1/4 ml-32">
+        <img src={image} alt="" />
+      </div>
+      <div className="w-2/3">
+        <h3 className="text-2xl font-bold ">Name: {Name}</h3>
+        <h4 className="text-xl font-semibold ">Ratings: {Rating}</h4>
+        <p className="text-xl">Comment: {Comment}</p>
       </div>
     </div>
   );
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -5,7 +5,7 @@ import ReviewDetail from "../ReviewDetail/ReviewDetail";
 const Reviews = () => {
   const [cart, setCart] = useReviews();
   return (
-    <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-4 items-center bg-gray-100">
+    <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-4 items-center ">
       {cart.map((product) => (
         <ReviewDetail key={product.id} product={product}></ReviewDetail>
       ))}
